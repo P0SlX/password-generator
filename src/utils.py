03682f2962dcb9148d5d73497ca3b61653a93e31
gaurondorian@gmail.com <gaurondorian@gmail.com>
2023-09-05 16:40:53 +0200

    :sparkles: Check stronger password

--- src/utils.py
+++ src/utils.py
@@ -9,18 +9,33 @@ def generer_mot_de_passe(longueur, inclure_chiffres, inclure_symboles):
         raise ValueError("La taille du mot de passe doit être supérieur à 0")
 
     # Base du mot de passe : lettres majuscules et minuscules
-    caracteres = string.ascii_letters
+    password = string.ascii_letters
     
     # Ajout des chiffres et des symboles si demandé
     if inclure_chiffres:
-        caracteres += string.digits
+        password += string.digits
 
     # Ajout des symboles si demandé
     if inclure_symboles:
-        caracteres += string.punctuation
+        password += string.punctuation
+
+    password = ''.join(random.choice(password) for _ in range(int(longueur)))
+
+    # Vérification que le mot de passe est sécurisé
+    if not any(c.isupper() for c in password):
+        raise ValueError("Le mot de passe doit contenir au moins une lettre majuscule")
+    if not any(c.islower() for c in password):
+        raise ValueError("Le mot de passe doit contenir au moins une lettre minuscule")
+    
+
+    if inclure_chiffres and not any(c.isdigit() for c in password):
+        raise ValueError("Le mot de passe doit contenir au moins un chiffre")
+    if inclure_symboles and not any(c in string.punctuation for c in password):
+        raise ValueError("Le mot de passe doit contenir au moins un symbole")
+
+    return password
 
     # Génération du mot de passe à partir de la base de caractères
-    return ''.join(random.choice(caracteres) for _ in range(int(longueur)))
 
 def hash_password(pwd):
     return sha256(pwd.encode('utf-8')).hexdigest()
