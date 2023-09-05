ad90b15cba4c7f0d27f20866eabda33a546c320a
gaurondorian@gmail.com <gaurondorian@gmail.com>
2023-09-05 14:30:50 +0200

    :recycle: Init testing

---
+++ tests/test_generate_password.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+class TestGeneratePassword(TestCase):
+
+    def test_only_number():
+        pass
+
+    def test_only_letter():
+        pass
+
+    def test_only_special_char():
+        pass
+
+    def test_length():
+        pass
+
+    def test_hash():
+        pass
---
+++ tests/test_inputs.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+class TestInputs(TestCase):
+    
+    @patch()
+    def test_input_letter():
+        pass
+
+    @patch()
+    def test_input_number():
+        pass
